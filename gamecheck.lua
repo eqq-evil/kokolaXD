505bdd0eebce626116ec2e63a75c7248348945cd
eqq-evil <eqq_evil@protonmail.com>
2022-02-24 21:26:17 +0100

    Update gamecheck.lua

--- gamecheck.lua
+++ gamecheck.lua
@@ -1,8 +1,9 @@
 local HttpService = game:GetService("HttpService")
-
-----game list:
-
 local GameList = "https://raw.githubusercontent.com/eqq-evil/kokolaXD/main/games/gamelist.json"
 local GameListJSON = HttpService:JSONDecode(game:HttpGet(GameList))
 
-print(GameListJSON.Prison_Life.PlaceID)
+if game.PlaceId == GameListJSON.game1.PlaceID then
+    loadstring(game:HttpGet(GameListJSON.game1.ScriptURL))()
+elseif game.PlaceId == GameListJSON.game2.PlaceID then
+    loadstring(game:HttpGet(GameListJSON.game2.ScriptURL))()
+end
