5968764321043a9094a8426a00402d215bbbbc46
eqq-evil <eqq_evil@protonmail.com>
2022-02-24 20:57:51 +0100

    Update gamecheck.lua

--- gamecheck.lua
+++
@@ -1,4 +0,0 @@
-local Http = game:GetService("HttpService")
-data = Http:JSONDecode(Http:GetAsync("https://raw.githubusercontent.com/eqq-evil/kokolaXD/main/games/gamelist.json"))
-
-print(data("155615604"))
